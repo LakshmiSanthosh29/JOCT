719f6eb76a614d06c2030953339818070ba00ab1
LakshmiSanthosh29 <chlakshmi2906@gmail.com>
2024-10-03 21:38:29 -0700

    day 1: task 2 and task 3

---
+++ src/L2simcalc.java
@@ -0,0 +1,13 @@
+public class L2simcalc {
+    public static void main(String[] args) {
+        System.out.println("Addition");
+        System.out.println("10+10=20");
+        System.out.println("Subtraction");
+        System.out.println("10-10=0");
+        System.out.println("Multiplication");
+        System.out.println("10*10=100");
+        System.out.println("Division");
+        System.out.println("10/10=1");
+
+    }
+}
