c7e68b7a261f6cd878668aebe7f5f5a0b854ddc1
LakshmiSanthosh29 <chlakshmi2906@gmail.com>
2024-10-09 17:07:54 -0700

    day 1: task 2 and task 3

---
+++ src/L3tabpf.java
@@ -0,0 +1,25 @@
+public class L3tabpf {
+    public static void main(String[] args) {
+        int t = 8;
+        System.out.printf("%d*%d=%d", t,1, t *1);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,2, t *2);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,3, t *3);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,4, t *4);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,5, t *5);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,6, t *6);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,7, t *7);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,8, t *8);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,9, t *9);
+        System.out.println();
+        System.out.printf("%d*%d=%d", t,10, t *10);
+        System.out.println();
+    }
+}
